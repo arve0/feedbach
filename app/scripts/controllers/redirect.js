e19047a2a3052716923de9d8b98dbbd0d9330fd5
arve0 <arve.seljebu@gmail.com>
2015-11-05 22:23:06 +0100

    remove app

--- app/scripts/controllers/redirect.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('feedbachApp')
-.controller('RedirectCtrl', function ($scope, $routeParams, fbUtils) {
-  // redirect users who go to /#/id -> /vote/#/id
-  fbUtils.go('vote/#' + $routeParams.id);
-});
--- app/scripts/services/survey.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('feedbachApp')
-.factory('Survey', function Survey($resource) {
-  return $resource('/api/survey/:id');
-});
